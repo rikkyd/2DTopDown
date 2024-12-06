9305e3f9748c05c078fd5cef14414a91d304ddd0
rikkyd <58492806+rikkyd@users.noreply.github.com>
2024-12-06 17:36:30 +0700

    d

--- 2D Top Down Shooter/Assets/Scripts/UI/CoinUI.cs
+++ 2D Top Down Shooter/Assets/Scripts/UI/CoinUI.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class CoinUI : MonoBehaviour
 {
-    public Text text;
+    public TMP_Text text;
     public ScriptableInteger coinScriptable;
 
     void Start()
